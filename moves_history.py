79dd2c44651c649379aa0f5020a68d58998f3068
Elie-1996 <44254485+Elie-1996@users.noreply.github.com>
2019-03-15 11:49:20 +0200

    Optimized Winning
    * Now does less time

--- moves_history.py
+++ moves_history.py
@@ -130,7 +130,7 @@ class AStarAlgorithm:
             list_for_min_states = [curr_min_state]
             copy_of_game_board = deepcopy(self.actual_game)
             copy_of_game_board.move_car(curr_min_state.car_name, curr_min_state.direction, curr_min_state.steps)
-            if self.check_winning():
+            if self.check_winning(copy_of_game_board):
                 self.print_steps(curr_min_state)
                 return True
 
@@ -145,7 +145,7 @@ class AStarAlgorithm:
                 copy_of_game_board = deepcopy(self.actual_game)
                 copy_of_game_board.move_car(another_min_state.car_name, another_min_state.direction, another_min_state.steps)
 
-                if self.check_winning():
+                if self.check_winning(copy_of_game_board):
                     # print(another_min_state.car_name)
                     # print(another_min_state.direction)
                     # print(another_min_state.steps)
@@ -204,11 +204,12 @@ class AStarAlgorithm:
                 return i
         return -1
 
-    def check_winning(self):
-        red_car_end_col = self.actual_game.cars_information.get('X').end_col
+    @staticmethod
+    def check_winning(copy_of_game_board: Board):
+        red_car_end_col = copy_of_game_board.cars_information.get('X').end_col
         range_index = 6 - red_car_end_col
         for i in range(1, range_index):
-            if self.actual_game.game_board[2][red_car_end_col + i] != '.':
+            if copy_of_game_board.game_board[2][red_car_end_col + i] != '.':
                 return False
         return True
 
